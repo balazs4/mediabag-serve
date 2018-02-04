9d82c987a854d508c8fb160a024e5d8e2e33a67a
balazs4 <balazs4web@gmail.com>
2018-02-04 17:09:26 +0100

    feat: add range support for streaming

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { createServer } = require('http');
-const { createReadStream } = require('fs');
+const { createReadStream, statSync } = require('fs');
 const { join } = require('path');
 const { check } = require('content-check');
 const readdir = require('recursive-readdir');
 
 createServer(async (req, res) => {
-  const { url = '/', method } = req;
+  const { url = '/', method, headers: { range } } = req;
   if (method !== 'GET') {
     res.end();
     return;
@@ -21,7 +21,25 @@ createServer(async (req, res) => {
   }
 
   const file = join(process.env.FOLDER, url);
-  const stream = createReadStream(file);
-  res.writeHead(200, { 'Content-Type': check(stream) });
-  stream.pipe(res);
+  if (range) {
+    console.log('streaming...');
+    const total = statSync(file).size;
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : total - 1;
+    const stream = createReadStream(file, { start, end });
+    res.writeHead(206, {
+      'Content-Type': check(stream),
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': end - start + 1
+    });
+    stream.pipe(res);
+    return;
+  } else {
+    const stream = createReadStream(file);
+    res.writeHead(200, { 'Content-Type': check(stream) });
+    stream.pipe(res);
+    return;
+  }
 }).listen(process.env.PORT);
